904a7f68e6f3d34128c0a26a7157c2d09a04f0bc
Davinder Singh <devheer1@gmail.com>
2018-01-23 10:27:57 +0530

    calculator.py

---
+++ caclulator.py
@@ -0,0 +1,26 @@
+#Calculaor Commands
+
+#Addition
+print("addition")
+a=2
+b=5
+c=a+b
+print(c)
+
+#subtraction
+a=10
+a*2
+
+
+#multiply by same number
+5**2
+
+
+#divide
+17/3
+
+#multiple addition
+(50-5*6)
+(50-5*6)/4
+
+
