0bde0acde9275e40192277b1befa1006255a45c7
Davinder Singh <devheer1@gmail.com>
2018-01-24 14:20:07 +0530

    HEllo

---
+++ strings.py
@@ -0,0 +1,28 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x="Dev"
+>>> x
+'Dev'
+>>> x + "is a good boy."
+'Devis a good boy.'
+>>> y= "i have 'bad habits'."
+>>> y
+"i have 'bad habits'."
+>>> x[0]
+'D'
+>>> x[3]
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    x[3]
+IndexError: string index out of range
+>>> x[-2]
+'e'
+>>> x[0:1]
+'D'
+>>> x[1:2]
+'e'
+>>> x[0:2]
+'De'
+>>> str(40)
+'40'
+>>> 
