6ecad329cccc266f9da8334777a6f216e1a0a8af
Davinder Singh <devheer1@gmail.com>
2018-01-29 10:11:31 +0530

    List

---
+++ list.py
@@ -0,0 +1,32 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> CAP=['MCA','MCA2']
+>>> CAP
+['MCA', 'MCA2']
+>>> Details=['Davinder Singh','22 years']
+>>> Details
+['Davinder Singh', '22 years']
+>>> CAP.append('MSC')
+>>> CAP
+['MCA', 'MCA2', 'MSC']
+>>> empty[]
+SyntaxError: invalid syntax
+>>> empty=[]
+>>> empty.append(1)
+>>> empty
+[1]
+>>> number=['1','2','3']
+>>> number
+['1', '2', '3']
+>>> number[:2]
+['1', '2']
+>>> x=[['a','b','c','d'],['1','2','3','4']]
+>>> x
+[['a', 'b', 'c', 'd'], ['1', '2', '3', '4']]
+>>> len([])=0
+SyntaxError: can't assign to function call
+>>> 'i' in 'DEv'
+False
+>>> 'D' in 'Dev'
+True
+>>> 
