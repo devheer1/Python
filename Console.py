30c53ba2476c83c585d4f304549e0beac303c8c4
Davinder Singh <devheer1@gmail.com>
2018-01-29 10:59:37 +0530

    Console

---
+++ Console.py
@@ -0,0 +1,46 @@
+>>> name = input("I am Davinder Singh")
+I am Davinder Singh 
+>>> Number = input("Enter your number")
+Enter your number 10
+>>> Number
+' 10'
+>>> Number = int(input("Enter your number"))
+Enter your number12
+>>> Number
+12
+>>> Number = str(input("Enter the number")
+		 10
+		 
+SyntaxError: invalid syntax
+>>> Number
+		 
+12
+>>> Number = str(input("Enter your number"))
+		 
+Enter your number10
+>>> Number
+		 
+'10'
+>>> import os
+>>> os.system('clr')
+
+Num=int(input("Enter any number"))
+Enet any number 10
+>>> print("Your number is",Num)
+Your number is 10
+>>>
+
+
+Num=input("What is your name")
+Enter your name ? Dev
+>>> print("Hello,How are you ?",Num)
+Hello Dev
+>>> 
+
+
+
+num=int(input("enter a number"))
+enter a number 12
+>>> num1=int(input("enter other number"))
+enter other number 10
+>>> sum=num+num1
