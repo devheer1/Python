3fb49f1827b48615ce2d984208443f5a34168007
Davinder Singh <devheer1@gmail.com>
2018-01-30 11:06:36 +0530

    Continue

---
+++ continue.py
@@ -0,0 +1,14 @@
+for n in range(2,10):
+...     if n % 2 == 0:
+...        print("Found an even number",n)
+...        continue
+...     print("Found a number",n)
+...
+Found an even number 2
+Found a number 3
+Found an even number 4
+Found a number 5
+Found an even number 6
+Found a number 7
+Found an even number 8
+Found a number 9
